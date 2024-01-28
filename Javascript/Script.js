e5c94e3bb80e72adb46ff59e4c0a461d88d213ab
Muchiri Mwangi <148215535+Eddy-Web93@users.noreply.github.com>
2024-01-28 17:26:53 +0300

    Add files via upload

---
+++ Javascript/Script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('.bx-icon')
+let navList = document.querySelector('.vertical')
+
+menu.addEventListener("click", function(){
+    navList.classList.toggle("open")
+} )
+navList.addEventListener("click", function(){
+    navList.classList.toggle("open")
+} )
